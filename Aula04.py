6983f8abf6c68cd1675a18d3094d15384aa3f645
Robert <robertvinicius123@hotmail.com>
2025-10-15 17:12:31 +0000

    Questões

---
+++ Aula04.py
@@ -0,0 +1,22 @@
+#1ª Questão: print('Robert Vinicius Silva Macedo'[0:6])
+#2ª Questão: print("Python é uma linguagem de programação"[13:22])
+#3ª Questão: print('Python'[::-1])
+#4ª Questão: print("abcdefg"[2:5])
+#5ª Questão: print('radar'[::-1])
+#6ª Questão: print('                  Sem espaços                     '.strip())
+
+''' 7ª Questão
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+print(f"Olá, {nome}! Você tem {idade} anos.")
+'''
+
+''' 8ª Questão
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+soma = num1 + num2
+print(f"A soma de {num1} e {num2} é {soma}.")
+'''
+
+frase = input('Digite alguma coisa: ')
+print(frase.replace('a', '*'))
